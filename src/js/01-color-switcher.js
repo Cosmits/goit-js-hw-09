22e446d9359ac1f1f4b3782b4b0b2bc630f40f56
Cosmits <salge.cos@gmail.com>
2023-04-30 17:42:29 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+};
 
+let intervalID;
+
+// css
+refs.btnStart.style.marginLeft = "calc(50% - 80px)";
+refs.btnStart.style.fontSize = "30px";
+refs.btnStop.style.fontSize = "30px";
+refs.btnStart.style.padding = "15px";
+refs.btnStop.style.padding = "15px";
+//css
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', startChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+//============================================================================
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function startChangeColor() {
+    intervalID = setInterval(changeBodyColor, 1000);
+
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+}
+
+function stopChangeColor() {
+    clearTimeout(intervalID);
+
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+}
+
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
